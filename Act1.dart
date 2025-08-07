2a13fd9ecbe42490b32376def7e2fdac4e047baf
Eldric22 <romanoeldric@gmail.com>
2025-08-07 09:37:38 +0000

    Understanding the Dart

---
+++ Act1.dart
@@ -0,0 +1,9 @@
+void main(){
+  String greet = 'Welcome';
+  dynamic name= 'Alice';
+  print ("$greet, $name!");
+  name =25;
+  print ("$greet, $name!");
+}
+
+// Since the dynamic variable can carry any type of values. adding it, the program can run without compilation error. 
