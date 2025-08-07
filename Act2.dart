d8744c96b98f44f9b6bca1c5f923ee52e66868ca
Eldric22 <romanoeldric@gmail.com>
2025-08-07 09:39:46 +0000

    Activity 2 : Understanding the Dart

---
+++ Act2.dart
@@ -0,0 +1,19 @@
+void main (){
+  double result = calculatePerimeter(10, 5, false);
+  print('The result is for Rectangle ' + result.toString());
+  
+  result = calculatePerimeter(7,0,true);
+  print('The result is for Square ' + result.toString());
+}
+
+double calculatePerimeter( double width, double height,bool isSquare){
+  double perimeter;
+  if (isSquare){
+    perimeter= 4*width;
+  }
+  else {
+   perimeter= 2*(width + height);
+  
+  }
+  return perimeter;
+}
